863ac5e64d75a8e40c791cc3758b1e20a39579ee
Gabriela Vasconcelos <20075050@H2WHK19UQ6NW.tld>
2022-08-22 11:25:52 -0500

    first commit. Added list of words.

---
+++ letter.py
@@ -0,0 +1,6 @@
+#words
+word_bank = ["avenue", "axiom", "bagpipes", "banjo", "bikini", "blizzard", "cobwebs", "crypt", "espionage", "fluffy", 
+"galaxy", "gossip", "hydrogen", "jelly", "jukebox", "keyhole", "queue", "quartz", "quiz", "rhythm",
+"sphinx", "stretch", "subway", "transcript", "transplant", "waltz", "wizard", "zipper", "zombie", "zodiac"]
+
+hints = ["This word typically comes after a street name."]
