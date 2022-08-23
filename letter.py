26c94d460231b6ee8a2021f7996447fd45b2a459
Gabriela Vasconcelos <20075050@H2WHK19UQ6NW.tld>
2022-08-23 11:47:06 -0500

    replacing random word with the hidden display word

--- letter.py
+++ letter.py
@@ -1,24 +1,56 @@
-import random
+# import random
 
-#words
-word_bank = ["avenue", "axiom", "bagpipes", "banjo", "bikini", "blizzard", "cobwebs", "crypt", "espionage", "fluffy", 
-"galaxy", "gossip", "hydrogen", "jelly", "jukebox", "keyhole", "queue", "quartz", "quiz", "rhythm",
-"sphinx", "stretch", "subway", "transcript", "transplant", "waltz", "wizard", "zipper", "zombie", "zodiac"]
+# #words
+# word_bank = ["avenue", "axiom", "bagpipes", "banjo", "bikini", "blizzard", "cobwebs", "crypt", "espionage", "fluffy", 
+# "galaxy", "gossip", "hydrogen", "jelly", "jukebox", "keyhole", "queue", "quartz", "quiz", "rhythm",
+# "sphinx", "stretch", "subway", "transcript", "transplant", "waltz", "wizard", "zipper", "zombie", "zodiac"]
 
-hint_bank = ["This word typically comes after a street name.", "something that is established as self evidently true.", 
-"Scottish instrument", "kind of like a guitar", "it's a type of swimsuit", "a very cold storm", "this goes with halloween and spiders",
-"it's underground", "government spying", "used to describe stuffed animals", "the milkyway is considered this", "[] girl. It's a popular TV show", 
-"the simplest atom", "the type of fish that stings", "old way of playing music", "has to do with a lock and key", 
-"another way to say line", "crystal people use for manifesting", "easier version of a test", "patterns in music and poetry", 
-"it has a woman's head and a lion's body", "you should do this before working out.", "a form of public transportation", "document of all your grades",
-"type of surgery that involves moving organs", "a classic ballroom dance", "think Harry Potter", "it's on most jackets", "they eat peoples brains", 
-"What's your star sign?"]
+# hint_bank = ["This word typically comes after a street name.", "something that is established as self evidently true.", 
+# "Scottish instrument", "kind of like a guitar", "it's a type of swimsuit", "a very cold storm", "this goes with halloween and spiders",
+# "it's underground", "government spying", "used to describe stuffed animals", "the milkyway is considered this", "[] girl. It's a popular TV show", 
+# "the simplest atom", "the type of fish that stings", "old way of playing music", "has to do with a lock and key", 
+# "another way to say line", "crystal people use for manifesting", "easier version of a test", "patterns in music and poetry", 
+# "it has a woman's head and a lion's body", "you should do this before working out.", "a form of public transportation", "document of all your grades",
+# "type of surgery that involves moving organs", "a classic ballroom dance", "think Harry Potter", "it's on most jackets", "they eat peoples brains", 
+# "What's your star sign?"]
 
 # generate random word and hint
-word = random.choice(range(len(word_bank)))
-hint = word
-guess = len((word_bank[word]))
-computer_word = []
-user_word = []
+# word = random.choice(range(len(word_bank)))
+# word = (word_bank[word]) #the actual word
+# print(word)
+# comp_word = word
+# comp_word[0, len()]
+
+
+# print(comp_word)
+# hint = (hint_bank[word])
+# guess = len((word_bank[word])) + 2
 
 #user prompts
+
+# word = "keyboard"
+
+# guess_word = []
+# for letter in word:
+#     guess_word.append(letter)
+#     guess_word.replace(letter, "-")
+
+
+
+# print(guess_word)
+
+
+word = list("gaby")
+
+for 
+
+word[0] = "-"
+print(word)
+
+
+
+
+
+
+#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
+
